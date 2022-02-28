edc46eebd7e8892454bcae7f49872fdf4039009b
Jason Sianandar <sianandarjason@gmail.com>
2022-02-28 20:06:42 +0700

    added services component

---
+++ src/components/Services/Service.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Service.css";
+
+const Service = () => {
+  return (
+    <>
+      <h1>OUR SERVICES</h1>
+      <section className="layout">
+        <div className="card">
+          <h1>Web Development</h1>
+        </div>
+
+        <div className="card">
+          <h1>Graphic Design</h1>
+        </div>
+
+        <div className="card">
+          <h1>Digital Marketing</h1>
+        </div>
+
+        <div className="card">
+          <h1>Music and Audio</h1>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Service;
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ImageSlider from "../../components/Slider/ImageSlider";
 import { SliderData } from "../../components/Slider/SliderData";
+import Service from "../../components/Services/Service";
 import "./Landing.css";
 
 const Landing = () => {
@@ -21,13 +22,13 @@ const Landing = () => {
           <p className="lead">This is a work in progress</p>
           <div className="buttons">
             <Link to="/register" className="btn btn-primary">
-              Sign Up
-            </Link>
-            <Link to="/login" className="btn btn-light">
-              Login
+              GET STARTED
             </Link>
           </div>
         </div>
+      </section>
+      <section className="landing_service">
+        <Service />
       </section>
     </div>
   );
